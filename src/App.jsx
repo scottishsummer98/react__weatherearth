e469c8c331efba9e10ddf33a8698445ce3108c84
scottishsummer98 <samiremail98@gmail.com>
2022-11-11 21:07:54 +0600

    'currentweatherdone'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './components/Search'
 import CurrentWeather from './components/CurrentWeather'
 import ForecastAccordion from './components/ForecastAccordion'
 
 const App = () => {
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(' ')
+
+    const currentWeatherFetch = fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=72aba88f1e3fe117f5b58b25269aa3ec&units=metric`,
+    )
+    const forecastFetch = fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=72aba88f1e3fe117f5b58b25269aa3ec&units=metric`,
+    )
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json()
+        const forcastResponse = await response[1].json()
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse })
+        setForecast({ city: searchData.label, ...forcastResponse })
+      })
+      .catch(console.log)
+  }
   return (
     <div>
-      <Search />
-      <CurrentWeather />
-      <ForecastAccordion />
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <ForecastAccordion data={forecast} />}
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
 
-function Search() {
-  return <div>Search</div>
+function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null)
+  const loadOptions = (inputValue) => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '85120ebd3amsh1fe76304d59ec78p121ffcjsna299c0e3c6a1',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+      },
+    }
+
+    return fetch(
+      `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      options,
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            }
+          }),
+        }
+      })
+      .catch((err) => console.error(err))
+  }
+  const handleOnChange = (searchData) => {
+    setSearch(searchData)
+    onSearchChange(searchData)
+  }
+  return (
+    <AsyncPaginate
+      placeholder="Search City"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  )
 }
 
 export default Search
